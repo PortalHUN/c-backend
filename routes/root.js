25146861968e8c53c77a028020ed31f230960897
PortalHUN <giotomi03@gmail.com>
2022-11-15 15:47:58 +0100

    Linux server thingy

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
 
-router.route("/").get((req, res) => {
-  res.json({ code: 200, message: "Welcome to the server!" });
-});
+router
+  .route("/")
+  .get((req, res) => {
+    res.json({ code: 200, message: "Welcome to the server!" });
+  })
+  .post((req, res) => {
+    const { Title, Body, UserId } = req.body;
+    if (!Title || !Body || !UserId)
+      return res.json({ code: 500, message: "Argument missing..." });
+    res.json({ code: 200, Title, Body, UserId });
+  });
 
 module.exports = router;
